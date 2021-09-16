255611a8a1024447c1b55c92dc65ccaf9916daaf
XiaoYao <290966751@qq.com>
2021-09-16 15:31:23 +0800

    fix 实体修复

--- wot-commons/wot-common-entity/src/main/java/com/wot/base/PageResult.java
+++ wot-commons/wot-common-entity/src/main/java/com/wot/base/PageResult.java
@@ -1,10 +1,5 @@
 package com.wot.base;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 import java.util.List;
 
@@ -13,10 +8,6 @@ import java.util.List;
  *
  * @author zlt
  */
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class PageResult<T> implements Serializable {
     private static final long serialVersionUID = -275582248840137389L;
     /**
